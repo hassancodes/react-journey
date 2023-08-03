99406904b55d8c0e1fe7be72af7b477bcae6ca25
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-03 11:17:22 -0400

    update

--- src/Pages/forms.jsx
+++ src/Pages/forms.jsx
@@ -2,13 +2,17 @@ import {useForm} from "react-hook-form";
 export const Form=()=>{
     // react-hook-form is used for handling errors and submission
     // yup is for validation
+    const {register,handleSubmit} = useForm();
+    const onSubmit=()=>{
+        console.log("Hello World");
+    }
     return(
-    <form>
-        <input type="text" placeholder="Enter Full Name"/>
-        <input type="email" placeholder="Enter Email"/>
-        <input type="text" placeholder="Enter Age"/>
-        <input type="password" placeholder="Enter Password"/>
-        <input type="password" placeholder="Confirm Password"/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Enter Full Name" {...register("Name")}/>
+        <input type="email" placeholder="Enter Email"{...register("Email")}/>
+        <input type="text" placeholder="Enter Age" {...register("Age")}/>
+        <input type="password" placeholder="Enter Password" {...register("Password")}/>
+        <input type="password" placeholder="Confirm Password" {...register("Cpassword")}/>
         <input type="submit" name="form-submit" id="submit" />
     </form>
     );
